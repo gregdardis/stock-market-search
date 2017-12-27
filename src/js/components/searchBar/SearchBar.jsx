0a20ac807640568bec737ba9e06c7acb86896428
Greg Dardis <dardis.greg@gmail.com>
2017-12-27 13:54:00 -0700

    added eslint linter

--- src/js/components/searchBar/SearchBar.jsx
+++ src/js/components/searchBar/SearchBar.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   render() {
     return (
-      <input type='text'></input>
+      <input type='text' />
     );
   }
 }
