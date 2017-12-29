d42aab9521982bcd859b2f9c2dbe9c0bfa09890f
Greg Dardis <dardis.greg@gmail.com>
2017-12-29 12:21:55 -0700

    send data back to server based on URL parameter

--- src/server.js
+++ src/server.js
@@ -3,19 +3,18 @@ const yahooFinance = require('yahoo-finance');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-const options = {
-  symbol: 'AAPL',
-  from: '2016-01-01', 
-  to: '2017-12-25',
-  period: 'm'
-};
-
-yahooFinance.historical(options).then(quotes => {
-  console.log(JSON.stringify(quotes[0], null, 2));
+app.get('/stocks/:symbol', (req, res) => {
+  const symbol = req.params.symbol;
+  const options = {
+    symbol,
+    from: '2016-01-01',
+    to: '2017-12-25',
+    period: 'm'
+  };
+  yahooFinance.historical(options).then(quotes => {
+    console.log(JSON.stringify(quotes[0], null, 2));
+    res.send(quotes);
+  });
 });
 
 const port = 3000;
