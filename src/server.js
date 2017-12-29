ac0c43df73da08af1cd13835b448092c77d37be0
Greg Dardis <dardis.greg@gmail.com>
2017-12-29 12:08:53 -0700

    fixed dumb eslint errors and got api data from yahoo finance

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const yahooFinance = require('yahoo-finance');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+const options = {
+  symbol: 'AAPL',
+  from: '2016-01-01', 
+  to: '2017-12-25',
+  period: 'm'
+};
+
+yahooFinance.historical(options).then(quotes => {
+  console.log(JSON.stringify(quotes[0], null, 2));
 });
 
 const port = 3000;
