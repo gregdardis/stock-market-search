021b63005545801807c0f4b88219c337a94ffd20
Greg Dardis <dardis.greg@gmail.com>
2017-12-29 11:52:33 -0700

    installed nodemon and set up a basic server

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}`);
+});
