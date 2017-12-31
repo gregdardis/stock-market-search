101bf061d507d23454d1cfeb6b9a6905f4dea732
Greg Dardis <dardis.greg@gmail.com>
2017-12-31 15:53:56 -0700

    storing dataItems in an object instead of array

--- src/reducers/dataItems.js
+++ src/reducers/dataItems.js
@@ -2,7 +2,7 @@ import {
   UPDATE_DATA_ITEMS
 } from '../actions';
 
-export const reducer = (state = [], action) => {
+export const reducer = (state = {}, action) => {
   switch (action.type) {
   case UPDATE_DATA_ITEMS:
     return action.dataItems;
