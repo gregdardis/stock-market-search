4cfd5f4df7e9868fb65c1eb04ac8f814b092974d
Greg Dardis <dardis.greg@gmail.com>
2018-01-12 16:34:22 -0700

    added empty search action to performSearch action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 export const UPDATE_SEARCH_TERM = 'UPDATE_SEARCH_TERM';
 export const CLEAR_SEARCH_TERM = 'CLEAR_SEARCH_TERM';
 export const PERFORM_SEARCH = 'PERFORM_SEARCH';
+export const EMPTY_SEARCH = 'EMPTY_SEARCH';
 
 export const UPDATE_DATA_ITEMS = 'UPDATE_DATA_ITEMS';
 
@@ -20,10 +21,17 @@ export const clearSearchTerm = () => ({
   type: CLEAR_SEARCH_TERM
 });
 
-export const performSearch = searchTerm => ({
-  type: PERFORM_SEARCH,
-  searchTerm
-});
+export const performSearch = searchTerm => {
+  if (searchTerm === '') {
+    return {
+      type: EMPTY_SEARCH
+    };
+  }
+  return {
+    type: PERFORM_SEARCH,
+    searchTerm
+  };
+};
 
 // export const fetchStockRequest = () => ({
 //   type: FETCH_STOCK_REQUEST
