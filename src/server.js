f9122b1999652aa46d1d168c3bed76c42f46c920
Greg Dardis <dardis.greg@gmail.com>
2018-01-13 15:43:28 -0700

    refactoring

--- src/server.js
+++ src/server.js
@@ -3,74 +3,6 @@ const yahooFinance = require('yahoo-finance');
 
 const app = express();
 
-const stock = {
-  companyName: 'Alphabet Inc Class C',
-  symbol: 'GOOG',
-  stockData: {
-    Open: {
-      value: 26.50,
-      optionalValue: null,
-      valueSuffix: '',
-      optionalValueSuffix: ''
-    },
-    High: {
-      value: 26.78,
-      optionalValue: null,
-      valueSuffix: '',
-      optionalValueSuffix: ''
-    },
-    Low: {
-      value: 25.90,
-      optionalValue: null,
-      valueSuffix: '',
-      optionalValueSuffix: ''
-    },
-    Div: {
-      value: 1.50,
-      optionalValue: 6.04,
-      valueSuffix: '',
-      optionalValueSuffix: '%'
-    },
-    'Mkt Cap': {
-      value: 1,
-      optionalValue: null,
-      valueSuffix: 'B',
-      optionalValueSuffix: ''
-    },
-    Volume: {
-      value: 403214,
-      optionalValue: 532423,
-      valueSuffix: '',
-      optionalValueSuffix: ''
-    },
-    'P/E Ratio': {
-      value: 50,
-      optionalValue: 0.02,
-      valueSuffix: '',
-      optionalValueSuffix: ''
-    },
-    ROE: {
-      value: 21,
-      optionalValue: null,
-      valueSuffix: '%',
-      optionalValueSuffix: ''
-    },
-    FCFY: {
-      value: 10,
-      optionalValue: null,
-      valueSuffix: '%',
-      optionalValueSuffix: ''
-    }
-  }
-};
-
-// summaryDetail: dayHigh, dayLow, open, marketCap, volume, averageVolume, 
-// dividendYield, dividendRate, trailingPE
-
-// financialData: returnOnEquity, freeCashFlow
-
-// defaultKeyStatistics: trailingEps
-
 const convertDecimalToPercent = decimal => (
   decimal * 100
 );
@@ -81,45 +13,87 @@ const calculateFcfy = (freeCashflow, marketCap) => {
   return convertDecimalToPercent(freeCashflowNum / marketCapNum);
 };
 
-const createStockDataEntry = (value, optionalValue, valueSuffix = '', optionalValueSuffix = '') => ({
-  value,
-  optionalValue,
-  valueSuffix,
-  optionalValueSuffix
-});
-
-const processStockData = quote => {
-  const { summaryDetail, financialData, defaultKeyStatistics } = quote;
-  const stockData = {
-    Open: createStockDataEntry(summaryDetail.open),
-    High: createStockDataEntry(summaryDetail.dayHigh),
-    Low: createStockDataEntry(summaryDetail.dayLow),
-    Div: createStockDataEntry(summaryDetail.dividendYield, summaryDetail.dividendRate, '', '%'),
-    'Mkt Cap': createStockDataEntry(summaryDetail.marketCap),
-    Volume: createStockDataEntry(summaryDetail.volume, summaryDetail.averageVolume),
-    'P/E Ratio': createStockDataEntry(summaryDetail.trailingPE, defaultKeyStatistics.trailingEps),
-    ROE: createStockDataEntry(convertDecimalToPercent(financialData.returnOnEquity), null, '%'),
-    FCFY: createStockDataEntry(calculateFcfy(financialData.freeCashflow, summaryDetail.marketCap), null, '%')
+const createStockDataEntry = (value, options = {}) => {
+  const {
+    optionalValue,
+    valueSuffix = '',
+    optionalValueSuffix = ''
+  } = options;
+  return {
+    value,
+    optionalValue,
+    valueSuffix,
+    optionalValueSuffix
   };
-  // console.log(stockData);
-  console.log(defaultKeyStatistics);
+};
+
+const processStockData = ({
+  summaryDetail,
+  financialData,
+  defaultKeyStatistics
+}) => {
+  const stockData = {
+    Open: createStockDataEntry(
+      summaryDetail.open
+    ),
+    High: createStockDataEntry(
+      summaryDetail.dayHigh
+    ),
+    Low: createStockDataEntry(
+      summaryDetail.dayLow
+    ),
+    Div: createStockDataEntry(
+      summaryDetail.dividendYield,
+      {
+        optionalValue: summaryDetail.dividendRate,
+        optionalValueSuffix: '%'
+      }
+    ),
+    'Mkt Cap': createStockDataEntry(summaryDetail.marketCap),
+    Volume: createStockDataEntry(
+      summaryDetail.volume,
+      {
+        optionalValue: summaryDetail.averageVolume
+      }
+    ),
+    'P/E Ratio': createStockDataEntry(
+      summaryDetail.trailingPE,
+      {
+        optionalValue: defaultKeyStatistics.trailingEps
+      }
+    ),
+    ROE: createStockDataEntry(
+      convertDecimalToPercent(financialData.returnOnEquity),
+      {
+        valueSuffix: '%'
+      }
+    ),
+    FCFY: createStockDataEntry(
+      calculateFcfy(financialData.freeCashflow, summaryDetail.marketCap),
+      {
+        valueSuffix: '%'
+      }
+    )
+  };
   return stockData;
 };
 
-const createStock = quote => ({
-  companyName: quote.price.shortName,
-  symbol: quote.price.symbol,
-  stockData: processStockData(quote)
-});
+const createStock = quote => {
+  const {
+    price,
+    summaryDetail,
+    financialData,
+    defaultKeyStatistics
+  } = quote;
+  return {
+    companyName: price.shortName,
+    symbol: price.symbol,
+    stockData: processStockData({ summaryDetail, financialData, defaultKeyStatistics })
+  };
+};
 
 app.get('/api/stocks/:symbol', (req, res) => {
   const symbol = req.params.symbol;
-  // const options = {
-  //   symbol,
-  //   from: '2016-01-01',
-  //   to: '2017-12-25',
-  //   period: 'm'
-  // };
   yahooFinance.quote({
     symbol,
     modules: ['summaryDetail', 'defaultKeyStatistics', 'financialData', 'price']
@@ -127,14 +101,70 @@ app.get('/api/stocks/:symbol', (req, res) => {
     quote => res.send(createStock(quote)),
     error => console.log(error)
   );
-
-  // yahooFinance.historical(options).then(quotes => {
-  //   console.log(JSON.stringify(quotes[0], null, 2));
-  //   res.send(quotes);
-  // });
 });
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`app is listening on port ${port}`);
 });
+
+// const stock = {
+//   companyName: 'Alphabet Inc Class C',
+//   symbol: 'GOOG',
+//   stockData: {
+//     Open: {
+//       value: 26.50,
+//       optionalValue: null,
+//       valueSuffix: '',
+//       optionalValueSuffix: ''
+//     },
+//     High: {
+//       value: 26.78,
+//       optionalValue: null,
+//       valueSuffix: '',
+//       optionalValueSuffix: ''
+//     },
+//     Low: {
+//       value: 25.90,
+//       optionalValue: null,
+//       valueSuffix: '',
+//       optionalValueSuffix: ''
+//     },
+//     Div: {
+//       value: 1.50,
+//       optionalValue: 6.04,
+//       valueSuffix: '',
+//       optionalValueSuffix: '%'
+//     },
+//     'Mkt Cap': {
+//       value: 1,
+//       optionalValue: null,
+//       valueSuffix: 'B',
+//       optionalValueSuffix: ''
+//     },
+//     Volume: {
+//       value: 403214,
+//       optionalValue: 532423,
+//       valueSuffix: '',
+//       optionalValueSuffix: ''
+//     },
+//     'P/E Ratio': {
+//       value: 50,
+//       optionalValue: 0.02,
+//       valueSuffix: '',
+//       optionalValueSuffix: ''
+//     },
+//     ROE: {
+//       value: 21,
+//       optionalValue: null,
+//       valueSuffix: '%',
+//       optionalValueSuffix: ''
+//     },
+//     FCFY: {
+//       value: 10,
+//       optionalValue: null,
+//       valueSuffix: '%',
+//       optionalValueSuffix: ''
+//     }
+//   }
+// };
