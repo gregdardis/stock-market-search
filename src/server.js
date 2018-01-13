78f8b9e328c993104c3697206626f10d67b40217
Greg Dardis <dardis.greg@gmail.com>
2018-01-13 14:53:51 -0700

    get all data from server required and return to client in proper format

--- src/server.js
+++ src/server.js
@@ -64,15 +64,70 @@ const stock = {
   }
 };
 
+// summaryDetail: dayHigh, dayLow, open, marketCap, volume, averageVolume, 
+// dividendYield, dividendRate, trailingPE
+
+// financialData: returnOnEquity, freeCashFlow
+
+// defaultKeyStatistics: trailingEps
+
+const convertDecimalToPercent = decimal => (
+  decimal * 100
+);
+
+const calculateFcfy = (freeCashflow, marketCap) => {
+  const freeCashflowNum = parseInt(freeCashflow);
+  const marketCapNum = parseInt(marketCap);
+  return convertDecimalToPercent(freeCashflowNum / marketCapNum);
+};
+
+const createStockDataEntry = (value, optionalValue, valueSuffix = '', optionalValueSuffix = '') => ({
+  value,
+  optionalValue,
+  valueSuffix,
+  optionalValueSuffix
+});
+
+const processStockData = quote => {
+  const { summaryDetail, financialData, defaultKeyStatistics } = quote;
+  const stockData = {
+    Open: createStockDataEntry(summaryDetail.open),
+    High: createStockDataEntry(summaryDetail.dayHigh),
+    Low: createStockDataEntry(summaryDetail.dayLow),
+    Div: createStockDataEntry(summaryDetail.dividendYield, summaryDetail.dividendRate, '', '%'),
+    'Mkt Cap': createStockDataEntry(summaryDetail.marketCap),
+    Volume: createStockDataEntry(summaryDetail.volume, summaryDetail.averageVolume),
+    'P/E Ratio': createStockDataEntry(summaryDetail.trailingPE, defaultKeyStatistics.trailingEps),
+    ROE: createStockDataEntry(convertDecimalToPercent(financialData.returnOnEquity), null, '%'),
+    FCFY: createStockDataEntry(calculateFcfy(financialData.freeCashflow, summaryDetail.marketCap), null, '%')
+  };
+  // console.log(stockData);
+  console.log(defaultKeyStatistics);
+  return stockData;
+};
+
+const createStock = quote => ({
+  companyName: quote.price.shortName,
+  symbol: quote.price.symbol,
+  stockData: processStockData(quote)
+});
+
 app.get('/api/stocks/:symbol', (req, res) => {
-  // const symbol = req.params.symbol;
+  const symbol = req.params.symbol;
   // const options = {
   //   symbol,
   //   from: '2016-01-01',
   //   to: '2017-12-25',
   //   period: 'm'
   // };
-  res.send(stock);
+  yahooFinance.quote({
+    symbol,
+    modules: ['summaryDetail', 'defaultKeyStatistics', 'financialData', 'price']
+  }).then(
+    quote => res.send(createStock(quote)),
+    error => console.log(error)
+  );
+
   // yahooFinance.historical(options).then(quotes => {
   //   console.log(JSON.stringify(quotes[0], null, 2));
   //   res.send(quotes);
