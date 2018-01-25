822d84cd960cd20c00ec0c1ec3127fc956bbaf58
Greg Dardis <dardis.greg@gmail.com>
2018-01-25 11:50:58 -0700

    renamed dataItem.js to stringFormatting.js in utils

--- src/client/components/dataItem/index.js
+++ src/client/components/dataItem/index.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import DataItem from './DataItem';
-import { formatValueFromStateAndProps } from '../../../utils/formatting/dataItem';
+import { formatValueFromStateAndProps } from '../../../utils/formatting/stringFormatting';
 
 const formatLabelFromProps = ownProps => {
   const label = ownProps.label;
