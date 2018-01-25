337305bdc021f88d17b12433fcae8e9421021eff
Greg Dardis <dardis.greg@gmail.com>
2018-01-25 11:08:07 -0700

    removed redundent formatDate tests

--- test/server/server.js
+++ test/server/server.js
@@ -84,26 +84,17 @@ export const padSingleDigitWithZeroTest = () => {
 
 export const formatDateTest = () => {
   const daysInMonth = {
-    january: 31,
     february: 28,
-    march: 31,
     april: 30,
-    may: 31,
-    june: 30,
-    july: 31,
-    august: 31,
-    september: 30,
-    october: 31,
-    november: 30,
     december: 31
   };
   const YEAR = 2018;
   describe('formatDate', () => {
-    it('should properly format January date on last day of month', () => {
-      const TEST_DATE_JANUARY = new Date(YEAR, 0, daysInMonth.january);
+    it('should properly format Jan date on first day of month', () => {
+      const TEST_DATE_JANUARY = new Date(YEAR, 0, 1);
       expect(formatDate(TEST_DATE_JANUARY))
         .to
-        .equal(`${YEAR}-01-${daysInMonth.january}`);
+        .equal(`${YEAR}-01-01`);
     });
     it('should properly format February date on last day of month', () => {
       const TEST_DATE_FEBRUARY = new Date(YEAR, 1, daysInMonth.february);
@@ -111,65 +102,23 @@ export const formatDateTest = () => {
         .to
         .equal(`${YEAR}-02-${daysInMonth.february}`);
     });
-    it('should properly format March date on last day of month', () => {
-      const TEST_DATE_MARCH = new Date(YEAR, 2, daysInMonth.march);
-      expect(formatDate(TEST_DATE_MARCH))
-        .to
-        .equal(`${YEAR}-03-${daysInMonth.march}`);
-    });
     it('should properly format April date on last day of month', () => {
       const TEST_DATE_APRIL = new Date(YEAR, 3, daysInMonth.april);
       expect(formatDate(TEST_DATE_APRIL))
         .to
         .equal(`${YEAR}-04-${daysInMonth.april}`);
     });
-    it('should properly format May date on last day of month', () => {
-      const TEST_DATE_MAY = new Date(YEAR, 4, daysInMonth.may);
-      expect(formatDate(TEST_DATE_MAY))
-        .to
-        .equal(`${YEAR}-05-${daysInMonth.may}`);
-    });
-    it('should properly format June date on last day of month', () => {
-      const TEST_DATE_JUNE = new Date(YEAR, 5, daysInMonth.june);
-      expect(formatDate(TEST_DATE_JUNE))
-        .to
-        .equal(`${YEAR}-06-${daysInMonth.june}`);
-    });
-    it('should properly format July date on last day of month', () => {
-      const TEST_DATE_JULY = new Date(YEAR, 6, daysInMonth.july);
-      expect(formatDate(TEST_DATE_JULY))
-        .to
-        .equal(`${YEAR}-07-${daysInMonth.july}`);
-    });
-    it('should properly format August date on last day of month', () => {
-      const TEST_DATE_AUGUST = new Date(YEAR, 7, daysInMonth.august);
-      expect(formatDate(TEST_DATE_AUGUST))
-        .to
-        .equal(`${YEAR}-08-${daysInMonth.august}`);
-    });
-    it('should properly format September date on last day of month', () => {
-      const TEST_DATE_SEPTEMBER = new Date(YEAR, 8, daysInMonth.september);
-      expect(formatDate(TEST_DATE_SEPTEMBER))
-        .to
-        .equal(`${YEAR}-09-${daysInMonth.september}`);
-    });
-    it('should properly format October date on last day of month', () => {
-      const TEST_DATE_OCTOBER = new Date(YEAR, 9, daysInMonth.october);
-      expect(formatDate(TEST_DATE_OCTOBER))
-        .to
-        .equal(`${YEAR}-10-${daysInMonth.october}`);
-    });
-    it('should properly format November date on last day of month', () => {
-      const TEST_DATE_NOVEMBER = new Date(YEAR, 10, daysInMonth.november);
-      expect(formatDate(TEST_DATE_NOVEMBER))
-        .to
-        .equal(`${YEAR}-11-${daysInMonth.november}`);
-    });
     it('should properly format December date on last day of month', () => {
       const TEST_DATE_DECEMBER = new Date(YEAR, 11, daysInMonth.december);
       expect(formatDate(TEST_DATE_DECEMBER))
         .to
         .equal(`${YEAR}-12-${daysInMonth.december}`);
+    });
+    it('should properly format a date in the middle of March', () => {
+      const TEST_DATE_MARCH = new Date(YEAR, 2, 14);
+      expect(formatDate(TEST_DATE_MARCH))
+        .to
+        .equal(`${YEAR}-03-14`);
     });
     it('should throw an error when given the empty string', () => {
       expect(() => {
