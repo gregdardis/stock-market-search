068b5ac6af6869b04ad0fe81adb84876096a8c26
Greg Dardis <dardis.greg@gmail.com>
2018-01-25 10:59:41 -0700

    removed redundent padSingleDigitWithZero tests

--- test/server/server.js
+++ test/server/server.js
@@ -12,65 +12,30 @@ export const padSingleDigitWithZeroTest = () => {
         .to
         .equal('00');
     });
+    it('should pad \'0\' with a 0', () => {
+      expect(padSingleDigitWithZero('0'))
+        .to
+        .equal('00');
+    });
     it('should pad \'1\' with a 0', () => {
       expect(padSingleDigitWithZero('1'))
         .to
         .equal('01');
     });
-    it('should pad 2 with a 0 and stringify', () => {
-      expect(padSingleDigitWithZero(2))
+    it('should pad 1 with a 0 and stringify', () => {
+      expect(padSingleDigitWithZero(1))
         .to
-        .equal('02');
-    });
-    it('should pad \'3\' with a 0', () => {
-      expect(padSingleDigitWithZero('3'))
-        .to
-        .equal('03');
-    });
-    it('should pad 4 with a 0 and stringify', () => {
-      expect(padSingleDigitWithZero(4))
-        .to
-        .equal('04');
-    });
-    it('should pad \'5\' with a 0', () => {
-      expect(padSingleDigitWithZero('5'))
-        .to
-        .equal('05');
-    });
-    it('should pad 6 with a 0 and stringify', () => {
-      expect(padSingleDigitWithZero(6))
-        .to
-        .equal('06');
-    });
-    it('should pad \'7\' with a 0', () => {
-      expect(padSingleDigitWithZero('7'))
-        .to
-        .equal('07');
-    });
-    it('should pad 8 with a 0 and stringify', () => {
-      expect(padSingleDigitWithZero(8))
-        .to
-        .equal('08');
-    });
-    it('should pad \'9\' with a 0', () => {
-      expect(padSingleDigitWithZero('9'))
-        .to
-        .equal('09');
+        .equal('01');
     });
     it('should not change 10 but stringify', () => {
       expect(padSingleDigitWithZero(10))
         .to
         .equal('10');
     });
-    it('should not change \'11\'', () => {
-      expect(padSingleDigitWithZero('11'))
+    it('should not change \'10\'', () => {
+      expect(padSingleDigitWithZero('10'))
         .to
-        .equal('11');
-    });
-    it('should not change 31', () => {
-      expect(padSingleDigitWithZero(31))
-        .to
-        .equal('31');
+        .equal('10');
     });
     it('should throw an error if given an object', () => {
       expect(() => {
