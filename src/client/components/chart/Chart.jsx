ecab33f36e789bc865757ff5ded7cbcbbc988c03
Greg Dardis <dardis.greg@gmail.com>
2018-01-25 10:24:42 -0700

    made constants for Chart.jsx

--- src/client/components/chart/Chart.jsx
+++ src/client/components/chart/Chart.jsx
@@ -12,13 +12,20 @@ import dateFormat from 'dateformat';
 
 import { addCommas } from '../../../utils/formatting/numberFormatting';
 import './chart.css';
+import {
+  CHART_HEIGHT,
+  CHART_WIDTH,
+  CHART_X_AXIS_DATE_FORMAT,
+  CHART_X_AXIS_MIN_TICK_GAP
+} from '../../../constants';
 
 const Chart = ({
   data
 }) => (
-  <LineChart width={ 1000 } height={ 400 } data={ data } className='chart'>
+  <LineChart width={ CHART_WIDTH } height={ CHART_HEIGHT } data={ data } className='chart'>
     <CartesianGrid strokeDashArray='3 3' />
-    <XAxis dataKey='date' tickFormatter={ date => dateFormat(date, 'mmm d') } minTickGap={ 30 }/>
+    <XAxis dataKey='date' tickFormatter={ date => dateFormat(date, CHART_X_AXIS_DATE_FORMAT) }
+      minTickGap={ CHART_X_AXIS_MIN_TICK_GAP } />
     <YAxis dataKey='price' domain={ ['auto', 'auto'] } tickFormatter={ addCommas } />
     <Tooltip />
     <Line type='monotone' dataKey='price' dot={ false } stroke='red' />
