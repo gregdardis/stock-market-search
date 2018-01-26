ccff7a37993d9c94bf87d05b9238839b3f703f2e
hailey123 <hailey.musselman@gmail.com>
2018-01-25 18:54:01 -0800

    added basic routes for home & glossary

--- src/client/components/app/App.jsx
+++ src/client/components/app/App.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
 import Home from '../home';
-
 import './app.css';
 
+// TODO: extract to file
+const Glossary = () => (
+  <div>Glossary Page</div>
+);
+
 const App = () => (
-  <div className='app'>
-    <Home />
-  </div>
+  <Router>
+    <div className='app'>
+      <ul className='nav'>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/glossary">Glossary</Link></li>
+      </ul>
+      <hr/>
+      <Route exact path="/" component={ Home }/>
+      <Route path="/glossary" component={ Glossary }/>
+    </div>
+  </Router>
 );
 
 export default App;
