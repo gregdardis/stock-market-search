2af46b9228f82a6f7f611b504d7e2f4738393049
Greg Dardis <dardis.greg@gmail.com>
2018-01-28 09:31:54 -0700

    added some definitions content

--- src/client/components/definition/definitionData.js
+++ src/client/components/definition/definitionData.js
@@ -2,7 +2,12 @@ import {
   DEFINITION_OPEN,
   DEFINITION_HIGH,
   DEFINITION_LOW,
-  DEFINITION_MARKET_CAP
+  DEFINITION_MARKET_CAP,
+  DEFINITION_VOLUME,
+  DEFINITION_DIVIDEND,
+  DEFINITION_PE_RATIO,
+  DEFINITION_ROE,
+  DEFINITION_FCFY
 } from '../../../constants';
 
 module.exports = Object.freeze({
@@ -11,5 +16,21 @@ module.exports = Object.freeze({
   [DEFINITION_LOW]: 'Lowest price at which a stock was traded during the trading day.',
   [DEFINITION_MARKET_CAP]: 'Market capitalization (market cap) is the total ' +
   'market value of a company\'s outstanding shares. ' +
-  'To calculate market cap, multiply the company\'s number of outstanding shares it\'s share price.'
+  'To calculate market cap, multiply the company\'s number of outstanding shares by it\'s share price.',
+  [DEFINITION_VOLUME]: 'The total number of a security that changed hands during the trading day. ' +
+  'Average volume is the average number of a security that changes hands during a trading day.',
+  [DEFINITION_DIVIDEND]: 'An amount of the company\'s profits which is paid to the ' +
+  'holders of that company\'s stock. Dividend is paid as a certain amount per share.' +
+  'Dividend yield is a percentage calculated as (dividend per share / price per share) * 100%.',
+  [DEFINITION_PE_RATIO]: 'Used for valuing a company, P/E ratio the ratio of ' +
+  'a company\'s share price to it\'s earnings per share. ' +
+  'A low P/E ratio can suggest a stock is undervalued, ' +
+  'but factoring in future growth rate and other metrics can say otherwise. ' +
+  'Earnings per share (abbrev. EPS) is net income per outstanding share of common stock.',
+  [DEFINITION_ROE]: 'How well the company generates returns on the investment from it\'s shareholders. ' +
+  'It is how much after tax profit the company earned in comparison to total shareholder equity. ' +
+  'Calculated as (net income / shareholder equity) * 100%.',
+  [DEFINITION_FCFY]: 'A comparison between free cash flow per share and share price. ' +
+  'Higher percentages mean the company is generating more free cash flow per share, which is appealing. ' +
+  'Calculated as (free cash flow per share / market price per share) * 100%.'
 });
--- src/client/components/glossary/Glossary.jsx
+++ src/client/components/glossary/Glossary.jsx
@@ -6,7 +6,12 @@ import {
   DEFINITION_OPEN,
   DEFINITION_HIGH,
   DEFINITION_LOW,
-  DEFINITION_MARKET_CAP
+  DEFINITION_MARKET_CAP,
+  DEFINITION_VOLUME,
+  DEFINITION_DIVIDEND,
+  DEFINITION_PE_RATIO,
+  DEFINITION_ROE,
+  DEFINITION_FCFY
 } from '../../../constants';
 
 const Glossary = () => (
@@ -26,6 +31,26 @@ const Glossary = () => (
     <Definition definition={ {
       title: DEFINITION_MARKET_CAP,
       definition: definitionData[DEFINITION_MARKET_CAP]
+    } }/>
+    <Definition definition={ {
+      title: DEFINITION_VOLUME,
+      definition: definitionData[DEFINITION_VOLUME]
+    } }/>
+    <Definition definition={ {
+      title: DEFINITION_DIVIDEND,
+      definition: definitionData[DEFINITION_DIVIDEND]
+    } }/>
+    <Definition definition={ {
+      title: DEFINITION_PE_RATIO,
+      definition: definitionData[DEFINITION_PE_RATIO]
+    } }/>
+    <Definition definition={ {
+      title: DEFINITION_ROE,
+      definition: definitionData[DEFINITION_ROE]
+    } }/>
+    <Definition definition={ {
+      title: DEFINITION_FCFY,
+      definition: definitionData[DEFINITION_FCFY]
     } }/>
   </div>
 );
