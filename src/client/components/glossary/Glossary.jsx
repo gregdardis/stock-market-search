3de9562c61bf4df5087bb0ea16b2a733b5232be9
Greg Dardis <dardis.greg@gmail.com>
2018-01-28 10:03:32 -0700

    made Definitions by mapping from array instead of each one individually

--- src/client/components/glossary/Glossary.jsx
+++ src/client/components/glossary/Glossary.jsx
@@ -14,45 +14,31 @@ import {
   DEFINITION_FCFY
 } from '../../../constants';
 
-const Glossary = () => (
-  <div>
-    <Definition definition={ {
-      title: DEFINITION_OPEN,
-      definition: definitionData[DEFINITION_OPEN]
-    } }/>
-    <Definition definition={ {
-      title: DEFINITION_HIGH,
-      definition: definitionData[DEFINITION_HIGH]
-    } }/>
-    <Definition definition={ {
-      title: DEFINITION_LOW,
-      definition: definitionData[DEFINITION_LOW]
-    } }/>
-    <Definition definition={ {
-      title: DEFINITION_MARKET_CAP,
-      definition: definitionData[DEFINITION_MARKET_CAP]
-    } }/>
-    <Definition definition={ {
-      title: DEFINITION_VOLUME,
-      definition: definitionData[DEFINITION_VOLUME]
-    } }/>
-    <Definition definition={ {
-      title: DEFINITION_DIVIDEND,
-      definition: definitionData[DEFINITION_DIVIDEND]
-    } }/>
-    <Definition definition={ {
-      title: DEFINITION_PE_RATIO,
-      definition: definitionData[DEFINITION_PE_RATIO]
-    } }/>
-    <Definition definition={ {
-      title: DEFINITION_ROE,
-      definition: definitionData[DEFINITION_ROE]
-    } }/>
-    <Definition definition={ {
-      title: DEFINITION_FCFY,
-      definition: definitionData[DEFINITION_FCFY]
-    } }/>
-  </div>
-);
+const Glossary = () => {
+  const definitions = [
+    DEFINITION_OPEN,
+    DEFINITION_HIGH,
+    DEFINITION_LOW,
+    DEFINITION_MARKET_CAP,
+    DEFINITION_VOLUME,
+    DEFINITION_DIVIDEND,
+    DEFINITION_PE_RATIO,
+    DEFINITION_ROE,
+    DEFINITION_FCFY
+  ];
+  return (
+    <div>
+      { definitions.map(definition =>
+        <Definition
+          key={ definition }
+          definition={{
+            title: definition,
+            definition: definitionData[definition]
+          } }
+        />
+      ) }
+    </div>
+  );
+};
 
 export default Glossary;
