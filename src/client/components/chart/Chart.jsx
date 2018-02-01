8d53214a8003094260ecd3f335e94d698a933322
Greg Dardis <dardis.greg@gmail.com>
2018-02-01 14:21:39 -0700

    filled out methods for tooltip, x axis, tick gap with constants

--- src/client/components/chart/Chart.jsx
+++ src/client/components/chart/Chart.jsx
@@ -15,11 +15,21 @@ import './chart.css';
 import {
   CHART_HEIGHT,
   CHART_WIDTH,
-  CHART_FORMAT_TOOLTIP_MAX,
+  CHART_FORMAT_TOOLTIP_ONE_MONTH_OR_LONGER,
   CHART_FORMAT_X_AXIS_MAX,
   CHART_FORMAT_X_AXIS_ONE_YEAR,
-  CHART_X_AXIS_MAX_MIN_TICK_GAP,
-  CHART_X_AXIS_ONE_YEAR_MIN_TICK_GAP,
+  CHART_X_AXIS_MIN_TICK_GAP_MAX,
+  CHART_X_AXIS_MIN_TICK_GAP_ONE_YEAR,
+  CHART_X_AXIS_MIN_TICK_GAP_FIVE_YEAR,
+  CHART_X_AXIS_MIN_TICK_GAP_THREE_MONTH,
+  CHART_X_AXIS_MIN_TICK_GAP_ONE_MONTH,
+  CHART_X_AXIS_MIN_TICK_GAP_FIVE_DAY,
+  CHART_FORMAT_X_AXIS_THREE_MONTH,
+  CHART_FORMAT_X_AXIS_ONE_MONTH,
+  CHART_FORMAT_X_AXIS_FIVE_YEAR,
+  CHART_X_AXIS_MIN_TICK_GAP_ONE_DAY,
+  CHART_FORMAT_X_AXIS_FIVE_DAY,
+  CHART_FORMAT_X_AXIS_ONE_DAY,
   TIME_PERIOD_FIVE_DAY,
   TIME_PERIOD_FIVE_YEAR,
   TIME_PERIOD_MAX,
@@ -37,20 +47,26 @@ const Chart = ({
   const getMinTickGap = () => {
     switch (chartTimePeriod) {
     case TIME_PERIOD_MAX:
-      return CHART_X_AXIS_MAX_MIN_TICK_GAP;
+      return CHART_X_AXIS_MIN_TICK_GAP_MAX;
     case TIME_PERIOD_ONE_YEAR:
+      return CHART_X_AXIS_MIN_TICK_GAP_ONE_YEAR;
     case TIME_PERIOD_FIVE_YEAR:
+      return CHART_X_AXIS_MIN_TICK_GAP_FIVE_YEAR;
     case TIME_PERIOD_THREE_MONTH:
+      return CHART_X_AXIS_MIN_TICK_GAP_THREE_MONTH;
     case TIME_PERIOD_ONE_MONTH:
+      return CHART_X_AXIS_MIN_TICK_GAP_ONE_MONTH;
     case TIME_PERIOD_FIVE_DAY:
+      return CHART_X_AXIS_MIN_TICK_GAP_FIVE_DAY;
     case TIME_PERIOD_ONE_DAY:
+      return CHART_X_AXIS_MIN_TICK_GAP_ONE_DAY;
     default:
-      return CHART_X_AXIS_ONE_YEAR_MIN_TICK_GAP;
+      return CHART_X_AXIS_MIN_TICK_GAP_ONE_YEAR;
     }
   };
 
   const getTooltipFormatOneMonthOrLonger = date =>
-    dateFormat(date, CHART_FORMAT_TOOLTIP_MAX);
+    dateFormat(date, CHART_FORMAT_TOOLTIP_ONE_MONTH_OR_LONGER);
 
   // TODO: UPDATE THIS METHOD ONCE WE HAVE INFO BY EVERY 5 MIN
   // every 5 minutes for 1 day
@@ -71,17 +87,23 @@ const Chart = ({
     }
   };
 
-  // format by a certain number of years depending on how far back the data goes for max
+  // X axis should have what day it is for 5 day, and 2 hour intervals for 1 day
   const getXAxisFormat = date => {
     switch (chartTimePeriod) {
     case TIME_PERIOD_MAX:
-    case TIME_PERIOD_FIVE_YEAR:
       return dateFormat(date, CHART_FORMAT_X_AXIS_MAX);
+    case TIME_PERIOD_FIVE_YEAR:
+      return dateFormat(date, CHART_FORMAT_X_AXIS_FIVE_YEAR);
     case TIME_PERIOD_ONE_YEAR:
+      return dateFormat(date, CHART_FORMAT_X_AXIS_ONE_YEAR);
     case TIME_PERIOD_THREE_MONTH:
+      return dateFormat(date, CHART_FORMAT_X_AXIS_THREE_MONTH);
     case TIME_PERIOD_ONE_MONTH:
+      return dateFormat(date, CHART_FORMAT_X_AXIS_ONE_MONTH);
     case TIME_PERIOD_FIVE_DAY:
+      return dateFormat(date, CHART_FORMAT_X_AXIS_FIVE_DAY);
     case TIME_PERIOD_ONE_DAY:
+      return dateFormat(date, CHART_FORMAT_X_AXIS_ONE_DAY);
     default:
       return dateFormat(date, CHART_FORMAT_X_AXIS_ONE_YEAR);
     }
