35ebbbc533f5f8bfec044f26f5271ddce068a6e1
Greg Dardis <dardis.greg@gmail.com>
2018-02-01 08:55:10 -0700

    fixed styling of definition, added some styling for glossary

--- src/client/components/glossary/Glossary.jsx
+++ src/client/components/glossary/Glossary.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 import Definition from '../definition';
 import definitionData from '../definition/definitionData';
+import './glossary.css';
 
 const Glossary = () => {
   const definitions = definitionData.getDefinitionsArray();
   return (
-    <div>
+    <div className='glossary'>
       { definitions.map(definition =>
         <Definition
           key={ definition.title }
