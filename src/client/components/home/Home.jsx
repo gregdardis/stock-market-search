3acde5e1ce63ff86133dbe59253f3ccd98f0af1f
hailey123 <hailey.musselman@gmail.com>
2018-04-02 08:49:02 -0700

    installed react-spinners and started putting it in Home component

--- src/client/components/home/Home.jsx
+++ src/client/components/home/Home.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+import { RingLoader } from 'react-spinners';
 
 import Search from '../search';
 import StockDataRegion from '../stockDataRegion';
 import './home.css';
 
-const Home = () => (
+const Home = ({ loading }) => (
   <div className='home'>
     <Search />
-    <StockDataRegion />
+    { loading ?
+      <div className='loader'>
+        <RingLoader
+          color={ '#123abc' }
+          loading={ true } />
+      </div> :
+      <StockDataRegion />
+    }
   </div>
 );
+Home.propTypes = {
+  loading: PropTypes.bool.isRequired
+};
 
 export default Home;
--- src/client/components/home/index.js
+++ src/client/components/home/index.js
@@ -1,3 +1,12 @@
+import { connect } from 'react-redux';
+
 import Home from './Home';
 
-export default Home;
+const mapStateToProps = state => {
+  // TODO: determine from the state if a stock is loading
+  return {
+    loading: true
+  };
+};
+
+export default connect(mapStateToProps)(Home);
