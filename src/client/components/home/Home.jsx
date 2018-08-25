fab5b06e20fd930fc41d28aaabbbb9b78d0c5fbf
hailey123 <hailey.musselman@gmail.com>
2018-08-25 09:27:56 -0700

    gave spinner region fixed height

--- src/client/components/home/Home.jsx
+++ src/client/components/home/Home.jsx
@@ -8,16 +8,23 @@ import StockDataRegion from '../stockDataRegion';
 import { THEME_COLOR_DARK1 } from '../../../constants';
 import './home.css';
 
+const SPINNER_SIZE = 20;
+
 const Home = ({ loading }) => (
   <div className='home'>
     <Search />
     <div className={ classnames({
       loader: true,
       hidden: !loading
-    }) }>
+    }) }
+    style={{
+      // The spinner is a little taller than it should be, so add 4px
+      height: SPINNER_SIZE + 4
+    }}>
       <ScaleLoader
         color={ THEME_COLOR_DARK1 }
-        loading={ loading } />
+        loading={ loading }
+        height={ SPINNER_SIZE }/>
     </div>
     <StockDataRegion />
   </div>
