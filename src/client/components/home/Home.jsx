4a4d6bbbf1f2f899045781e80189236ee3b899d1
hailey123 <hailey.musselman@gmail.com>
2018-08-26 09:32:45 -0700

    changing loader based on whether initial fetch or sync

--- src/client/components/home/Home.jsx
+++ src/client/components/home/Home.jsx
@@ -1,37 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import { ScaleLoader } from 'react-spinners';
+import { BeatLoader, ScaleLoader } from 'react-spinners';
 
 import Search from '../search';
 import StockDataRegion from '../stockDataRegion';
 import { THEME_COLOR_DARK1 } from '../../../constants';
 import './home.css';
 
-const SPINNER_SIZE = 20;
+const STATUS_REGION_HEIGHT = 24;
 
-const Home = ({ loading }) => (
+const Loader = ({ loading, showingCachedStock }) => {
+  const loaderProps = {
+    loading: loading,
+    color: THEME_COLOR_DARK1
+  };
+  return showingCachedStock
+    ? <BeatLoader
+      // - 12 because it's a size that looks good
+      size = { STATUS_REGION_HEIGHT - 12 }
+      { ...loaderProps } />
+    : <ScaleLoader
+      // - 4 because it's a height that looks good
+      height={ STATUS_REGION_HEIGHT - 4 }
+      { ...loaderProps } />;
+};
+Loader.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  showingCachedStock: PropTypes.bool
+};
+
+const Home = ({
+  loading,
+  showingCachedStock
+}) => (
   <div className='home'>
     <Search />
     <div className={ classnames({
       loader: true,
       hidden: !loading
     }) }
-    style={{
-      // The loader is a little taller than it should be,
-      // so add 4px to its container's height
-      height: SPINNER_SIZE + 4
-    }}>
-      <ScaleLoader
-        color={ THEME_COLOR_DARK1 }
+    style={{ height: STATUS_REGION_HEIGHT }}>
+      <Loader
         loading={ loading }
-        height={ SPINNER_SIZE }/>
+        showingCachedStock={ showingCachedStock }/>
     </div>
     <StockDataRegion />
   </div>
 );
 Home.propTypes = {
-  loading: PropTypes.bool.isRequired
+  loading: PropTypes.bool.isRequired,
+  showingCachedStock: PropTypes.bool
 };
 
 export default Home;
--- src/client/components/home/index.js
+++ src/client/components/home/index.js
@@ -2,17 +2,31 @@ import { connect } from 'react-redux';
 
 import Home from './Home';
 
-const isStockLoading = state => {
+function isStockLoading(state) {
   const { search, stocks } = state;
   const { lastSearch } = search;
   if (lastSearch && stocks[lastSearch]) {
     return stocks[lastSearch].isFetching;
   }
   return false;
-};
+}
 
-const mapStateToProps = state => ({
-  loading: isStockLoading(state)
-});
+function searchedStockSelected(state) {
+  const { search, stocks } = state;
+  const { lastSearch } = search;
+  if (lastSearch && stocks[lastSearch].symbol) {
+    return stocks[lastSearch].symbol === state.selectedStock;
+  }
+  return false;
+}
+
+const mapStateToProps = state => {
+  const loading = isStockLoading(state);
+
+  return {
+    loading,
+    showingCachedStock: loading && searchedStockSelected(state)
+  };
+};
 
 export default connect(mapStateToProps)(Home);
