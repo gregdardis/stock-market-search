6a9e1d674bcd85a240c06d65466d6bee54e3c01a
Greg Dardis <dardis.greg@gmail.com>
2018-09-04 16:44:51 -0700

    tested createStockDataEntry

--- src/utils/apiUtils/chartData.js
+++ src/utils/apiUtils/chartData.js
@@ -53,14 +53,52 @@ export function calculateFcfy(freeCashflow, marketCap) {
 }
 
 export function createStockDataEntry(value, options = {}) {
+  const parsedValue = parseInt(value);
+  if (isNaN(value) || isNaN(parsedValue)) {
+    throw new TypeError(
+      `${createStockDataEntry.name} requires a number ` +
+      'for the value parameter.'
+    );
+  }
+
   const {
     optionalValue,
     valueFormat = NUMBER_FORMAT_DEFAULT,
     optionalValueFormat = NUMBER_FORMAT_DEFAULT
   } = options;
+
+  let parsedOptionalValue;
+  if (optionalValue) {
+    parsedOptionalValue = parseInt(optionalValue);
+    if (isNaN(optionalValue) || isNaN(parsedOptionalValue)) {
+      throw new TypeError(
+        `${createStockDataEntry.name} requires a number ` +
+        'for the optionalValue option.'
+      );
+    }
+  }
+
+  if (
+    typeof valueFormat !== 'string'
+    && !(valueFormat instanceof String)
+  ) {
+    throw new TypeError(
+      `${createStockDataEntry.name} requires a string for ` +
+      'the valueFormat option'
+    );
+  }
+  if (
+    typeof optionalValueFormat !== 'string'
+    && !(optionalValueFormat instanceof String)
+  ) {
+    throw new TypeError(
+      `${createStockDataEntry.name} requires a string for ` +
+      'the optionalValueFormat option'
+    );
+  }
   return {
-    value,
-    optionalValue,
+    value: parsedValue,
+    optionalValue: parsedOptionalValue,
     valueFormat,
     optionalValueFormat
   };
