84cb6bdb3b636ca7ddbd78b1b6451005f61c13a9
hailey123 <hailey.musselman@gmail.com>
2018-09-09 10:15:35 -0700

    added test for stockReducer RECEIVE_STOCK

--- src/client/reducers/stock.test.js
+++ src/client/reducers/stock.test.js
@@ -1,22 +1,45 @@
 import { expect } from 'chai';
+import sinon from 'sinon';
 
 import { reducer } from './stock';
 import { RECEIVE_STOCK } from '../actions';
+import { mockStockData } from '../testData';
 
 
 /* eslint-disable no-undefined */
 
 describe('stock reducer', () => {
-  it('should return the initial state', () => {
-    const initialState = {
+  let initialState;
+  beforeEach(() => {
+    initialState = {
       companyName: '',
       symbol: '',
       exchange: '',
       stockOverviewData: {}
     };
+  });
+
+  it('should return the initial state', () => {
     expect(reducer(undefined, {})).to.deep.equal(initialState);
   });
   it('should handle RECEIVE_STOCK', () => {
-    // TODO
+    const clock = sinon.useFakeTimers();
+    const receivedAt = new Date();
+
+    expect(
+      reducer(
+        initialState,
+        {
+          type: RECEIVE_STOCK,
+          receivedAt,
+          ...mockStockData
+        }
+      )
+    ).to.deep.equal({
+      ...mockStockData,
+      lastUpdated: receivedAt
+    });
+
+    clock.restore();
   });
 });
