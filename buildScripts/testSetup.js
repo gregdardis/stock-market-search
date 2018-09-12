75b5179d703116fa58434dd2fab08f8e8ed4310f
Greg Dardis <dardis.greg@gmail.com>
2018-09-12 08:51:10 -0700

    swapped out mocha for jest

--- buildScripts/testSetup.js
+++ buildScripts/testSetup.js
@@ -3,9 +3,6 @@
 // Register babel to transpile before our tests run.
 require('babel-register')();
 
-// Disable webpack features that Mocha doesn't understand.
-require.extensions['.css'] = function () {};
-
 const enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
