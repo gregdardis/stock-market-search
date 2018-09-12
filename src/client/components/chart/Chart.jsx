5e0dfb06e533c83def96db40d9c3f089262c7f96
Greg Dardis <dardis.greg@gmail.com>
2018-09-12 14:51:57 -0700

    moved all Chart.jsx private methods to utils module

--- src/client/components/chart/Chart.jsx
+++ src/client/components/chart/Chart.jsx
@@ -9,56 +9,41 @@ import {
   XAxis,
   YAxis
 } from 'recharts';
-import numeral from 'numeral';
 
 import { addCommas } from '../../../utils/formatting/numberFormatting';
 import './chart.css';
-import {
-  CHART_DATA_KEY_Y_AXIS,
-  CHART_META_DATA,
-  NUMBER_FORMAT_PRICE
-} from '../../../constants/formatting';
+import { CHART_DATA_KEY_Y_AXIS } from '../../../constants/formatting';
 import { CHART_LINE_COLOR } from '../../../constants/colors';
 import {
-  getTooltipLabelFormatter
+  formatAsPrice,
+  getTooltipLabelFormatter,
+  getXAxisDataKey,
+  getXAxisMinTickGap,
+  getXAxisTickFormatter
 } from '../../../utils/chartUtils';
 
 export const Chart = ({
   chartTimePeriodIndex,
   data
-}) => {
-  const getXAxisDataKey = () =>
-    CHART_META_DATA[chartTimePeriodIndex].xAxisDataKey;
-
-  const getXAxisMinTickGap = () =>
-    CHART_META_DATA[chartTimePeriodIndex].xAxisMinTickGap;
-
-  const getXAxisTickFormatter = () =>
-    CHART_META_DATA[chartTimePeriodIndex].getXAxisTickFormatter;
-
-  const formatAsPrice = value =>
-    numeral(value).format(NUMBER_FORMAT_PRICE);
-
-  return (
-    <ResponsiveContainer height='65%' width='85%'>
-      <LineChart data={ data }
-        className='chart' >
-        <CartesianGrid strokeDashArray='3 3' />
-        <XAxis dataKey={ getXAxisDataKey() }
-          tickFormatter={ getXAxisTickFormatter() }
-          minTickGap={ getXAxisMinTickGap() } />
-        <YAxis dataKey={ CHART_DATA_KEY_Y_AXIS } domain={ ['auto', 'auto'] }
-          tickFormatter={ addCommas } />
-        <Tooltip
-          labelFormatter={ getTooltipLabelFormatter(chartTimePeriodIndex) }
-          formatter={ price => formatAsPrice(price) }
-          separator=': ' />
-        <Line type='monotone' dataKey={ CHART_DATA_KEY_Y_AXIS } dot={ false }
-          stroke={ CHART_LINE_COLOR } isAnimationActive={ false } />
-      </LineChart>
-    </ResponsiveContainer>
-  );
-};
+}) => (
+  <ResponsiveContainer height='65%' width='85%'>
+    <LineChart data={ data }
+      className='chart' >
+      <CartesianGrid strokeDashArray='3 3' />
+      <XAxis dataKey={ getXAxisDataKey(chartTimePeriodIndex) }
+        tickFormatter={ getXAxisTickFormatter(chartTimePeriodIndex) }
+        minTickGap={ getXAxisMinTickGap(chartTimePeriodIndex) } />
+      <YAxis dataKey={ CHART_DATA_KEY_Y_AXIS } domain={ ['auto', 'auto'] }
+        tickFormatter={ addCommas } />
+      <Tooltip
+        labelFormatter={ getTooltipLabelFormatter(chartTimePeriodIndex) }
+        formatter={ price => formatAsPrice(price) }
+        separator=': ' />
+      <Line type='monotone' dataKey={ CHART_DATA_KEY_Y_AXIS } dot={ false }
+        stroke={ CHART_LINE_COLOR } isAnimationActive={ false } />
+    </LineChart>
+  </ResponsiveContainer>
+);
 Chart.propTypes = {
   chartTimePeriodIndex: PropTypes.number.isRequired,
   data: PropTypes.array.isRequired
--- src/utils/chartUtils.js
+++ src/utils/chartUtils.js
@@ -1,5 +1,26 @@
-import { CHART_META_DATA } from '../constants/formatting';
+import numeral from 'numeral';
+
+import {
+  CHART_META_DATA,
+  NUMBER_FORMAT_PRICE
+} from '../constants/formatting';
 
 export const getTooltipLabelFormatter = chartTimePeriodIndex => (
   CHART_META_DATA[chartTimePeriodIndex].getTooltipLabelFormatter
+);
+
+export const getXAxisDataKey = chartTimePeriodIndex => (
+  CHART_META_DATA[chartTimePeriodIndex].xAxisDataKey
+);
+
+export const getXAxisMinTickGap = chartTimePeriodIndex => (
+  CHART_META_DATA[chartTimePeriodIndex].xAxisMinTickGap
+);
+
+export const getXAxisTickFormatter = chartTimePeriodIndex => (
+  CHART_META_DATA[chartTimePeriodIndex].getXAxisTickFormatter
+);
+
+export const formatAsPrice = value => (
+  numeral(value).format(NUMBER_FORMAT_PRICE)
 );
