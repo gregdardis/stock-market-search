1ec7a981b8c7ad17de95f94ab88cfc2aa1ca9bd0
Greg Dardis <dardis.greg@gmail.com>
2018-09-12 15:24:26 -0700

    tested getTooltipLabelFormatter

--- src/utils/chartUtils.test.js
+++ src/utils/chartUtils.test.js
@@ -1,5 +1,20 @@
-describe('chartUtils', () => {
-  test('true is true', () => {
-    expect(true).toEqual(true);
+import {
+  getTooltipLabelFormatter
+} from './chartUtils';
+import * as dateFormatting from './dateUtils/dateFormatting';
+
+describe('getTooltipLabelFormatter', () => {
+  it('gets the tooltip label formatter for index 1', () => {
+    const mockTryFormatDate = jest.spyOn(
+      dateFormatting,
+      'tryFormatDate'
+    );
+    const fn = getTooltipLabelFormatter(1);
+
+    fn('09-14-18');
+
+    expect(mockTryFormatDate).toHaveBeenCalledTimes(1);
+
+    mockTryFormatDate.mockReset();
   });
 });
