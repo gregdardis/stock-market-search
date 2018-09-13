0e8a6ed4ca20b93144a16f2b5956c4b3cab7c3bb
Greg Dardis <dardis.greg@gmail.com>
2018-09-13 07:47:46 -0700

    made getTooltipLabelFormatter require a number

--- src/utils/chartUtils.js
+++ src/utils/chartUtils.js
@@ -6,8 +6,9 @@ import {
 } from '../constants/formatting';
 
 export const getTooltipLabelFormatter = chartTimePeriodIndex => {
-  if (chartTimePeriodIndex < 0 ||
-      chartTimePeriodIndex >= CHART_META_DATA.length) {
+  if (typeof chartTimePeriodIndex !== 'number'
+      || chartTimePeriodIndex < 0
+      || chartTimePeriodIndex >= CHART_META_DATA.length) {
     return null;
   }
   return CHART_META_DATA[chartTimePeriodIndex].getTooltipLabelFormatter;
@@ -21,6 +22,7 @@ export const getXAxisDataKey = chartTimePeriodIndex => {
   return CHART_META_DATA[chartTimePeriodIndex].xAxisDataKey;
 };
 
+// TODO: return null
 export const getXAxisMinTickGap = chartTimePeriodIndex => (
   CHART_META_DATA[chartTimePeriodIndex].xAxisMinTickGap
 );
--- src/utils/chartUtils.test.js
+++ src/utils/chartUtils.test.js
@@ -26,8 +26,42 @@ describe('getTooltipLabelFormatter', () => {
   it('returns null if chartTimePeriodIndex is too high', () => {
     expect(getTooltipLabelFormatter(CHART_META_DATA.length)).toBeNull();
   });
+  it('returns null if chartTimePeriodIndex is a string', () => {
+    expect(getTooltipLabelFormatter('hello')).toBeNull();
+  });
+  it('returns null if chartTimePeriodIndex is a numeric string', () => {
+    expect(getTooltipLabelFormatter('5')).toBeNull();
+  });
+  it('returns null if chartTimePeriodIndex is a' +
+     'number followed by letters', () => {
+    expect(getTooltipLabelFormatter('5hello')).toBeNull();
+  });
+  it('returns null if chartTimePeriodIndex is' +
+     'letters followed by a number', () => {
+    expect(getTooltipLabelFormatter('hello5')).toBeNull();
+  });
+  it('returns null if chartTimePeriodIndex is an object', () => {
+    expect(getTooltipLabelFormatter({})).toBeNull();
+  });
+  it('returns null if chartTimePeriodIndex is an array', () => {
+    expect(getTooltipLabelFormatter([])).toBeNull();
+  });
+  it('returns null if chartTimePeriodIndex is a boolean', () => {
+    expect(getTooltipLabelFormatter(true)).toBeNull();
+  });
+  it('returns null if chartTimePeriodIndex is null', () => {
+    expect(getTooltipLabelFormatter(null)).toBeNull();
+  });
+  it('returns null if chartTimePeriodIndex is undefined', () => {
+    /* eslint-disable no-undefined*/
+    expect(getTooltipLabelFormatter(undefined)).toBeNull();
+  });
+  it('returns null if chartTimePeriodIndex is a function', () => {
+    expect(getTooltipLabelFormatter(() => {})).toBeNull();
+  });
 });
 
+// TODO: check if !number
 describe('getXAxisDataKey', () => {
   it('gets xAxisDataKey from CHART_META_DATA', () => {
     expect(getXAxisDataKey(0)).toBe('dateAndTime');
