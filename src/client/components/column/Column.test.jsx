1664869a89b8bd4cca70a7ae51ecf11efb13b56d
Greg Dardis <dardis.greg@gmail.com>
2018-09-14 10:23:38 -0700

    added more Column tests

--- src/client/components/column/Column.test.jsx
+++ src/client/components/column/Column.test.jsx
@@ -23,11 +23,6 @@ describe('<Column />', () => {
     }]
   };
 
-  const propsEmptyComponentsProps = {
-    ...baseProps,
-    componentsProps: []
-  };
-
   let wrapper;
 
   it('has expected className for styling', () => {
@@ -36,16 +31,49 @@ describe('<Column />', () => {
     );
     expect(wrapper).to.have.className('column');
   });
-  it('renders 2 MockCellComponent\'s', () => {
+  it('renders 2 MockCellComponent\'s, with showBottomBorder false', () => {
     wrapper = shallow(
       <Column { ...baseProps } />
     );
     expect(wrapper.find(MockCellComponent)).to.have.length(2);
+    expect(wrapper.find('MockCellComponent').at(0)
+      .prop('showBottomBorder')).to.equal(false);
+    expect(wrapper.find('MockCellComponent').at(1)
+      .prop('showBottomBorder')).to.equal(false);
   });
   it('renders 0 MockCellComponents if empty componentsProps', () => {
+    const propsEmptyComponentsProps = {
+      ...baseProps,
+      componentsProps: []
+    };
     wrapper = shallow(
       <Column { ...propsEmptyComponentsProps } />
     );
     expect(wrapper.find(MockCellComponent)).to.have.length(0);
+  });
+  it('still renders with columnKeyName that isn\'t ' +
+     'a field in cellProps', () => {
+    const propsBadColumnKeyName = {
+      ...baseProps,
+      columnKeyName: 'bad'
+    };
+    wrapper = shallow(
+      <Column { ...propsBadColumnKeyName } />
+    );
+    expect(wrapper.find(MockCellComponent)).to.have.length(2);
+  });
+  it('has showBottomBorder = true if cellShouldShowBottomBorder ' +
+     'returns true', () => {
+    const propsShowBottomBorder = {
+      ...baseProps,
+      cellShouldShowBottomBorder: () => true
+    };
+    wrapper = shallow(
+      <Column { ...propsShowBottomBorder } />
+    );
+    expect(wrapper.find('MockCellComponent').at(0)
+      .prop('showBottomBorder')).to.equal(true);
+    expect(wrapper.find('MockCellComponent').at(1)
+      .prop('showBottomBorder')).to.equal(true);
   });
 });
