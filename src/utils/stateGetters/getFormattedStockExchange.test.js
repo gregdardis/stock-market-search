b5d6ebd8c4b51aa7e048c6bfe80c43a7059ad712
Greg Dardis <dardis.greg@gmail.com>
2018-09-15 09:32:09 -0700

    calls stubbed function once

--- src/utils/stateGetters/getFormattedStockExchange.test.js
+++ src/utils/stateGetters/getFormattedStockExchange.test.js
@@ -4,20 +4,34 @@ import {
 } from './getFormattedStockExchange';
 
 describe('getFormattedStockExchange', () => {
-  const getSelectedStockValueForKeyStub = jest.spyOn(
-    stateGetters,
-    'getSelectedStockValueForKey'
-  );
+  let mockGetSelectedStockValueForKey;
+  beforeAll(() => {
+    mockGetSelectedStockValueForKey = jest.spyOn(
+      stateGetters,
+      'getSelectedStockValueForKey'
+    );
+  });
+
+  afterAll(() => {
+    mockGetSelectedStockValueForKey.mockRestore();
+  });
+
+  it('calls getSelectedStockValueForKey once', () => {
+    mockGetSelectedStockValueForKey.mockReturnValue('Toronto');
+    getFormattedStockExchange({});
+    expect(mockGetSelectedStockValueForKey).toHaveBeenCalledTimes(1);
+  });
   it('returns TSX if given Toronto', () => {
-    getSelectedStockValueForKeyStub.mockReturnValue('Toronto');
+    mockGetSelectedStockValueForKey.mockReturnValue('Toronto');
     expect(getFormattedStockExchange({})).toEqual('TSX');
   });
   it('returns NASDAQ if given NasdaqGS', () => {
-    getSelectedStockValueForKeyStub.mockReturnValue('NasdaqGS');
+    mockGetSelectedStockValueForKey.mockReturnValue('NasdaqGS');
     expect(getFormattedStockExchange({})).toEqual('NASDAQ');
   });
   it('returns the exchange given if anything else', () => {
-    getSelectedStockValueForKeyStub.mockReturnValue('NYSE');
+    mockGetSelectedStockValueForKey.mockReturnValue('NYSE');
     expect(getFormattedStockExchange({})).toEqual('NYSE');
   });
+
 });
