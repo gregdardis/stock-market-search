774ff519bb9ac593dcce747d360da267dd83a924
Greg Dardis <dardis.greg@gmail.com>
2018-09-16 07:35:48 -0700

    return Glossary component without curly braces or return statement

--- src/client/components/glossary/Glossary.jsx
+++ src/client/components/glossary/Glossary.jsx
@@ -4,20 +4,18 @@ import Definition from '../definition';
 import { DEFINITIONS } from '../definition/definitionData';
 import './glossary.css';
 
-const Glossary = () => {
-  return (
-    <div className='glossary'>
-      { DEFINITIONS.map(definition =>
-        <Definition
-          key={ definition.title }
-          definition={ {
-            title: definition.title,
-            definition: definition.definition,
-            equations: definition.equations
-          } }
-        />
-      ) }
-    </div>
-  );
-};
+const Glossary = () => (
+  <div className='glossary'>
+    { DEFINITIONS.map(definition =>
+      <Definition
+        key={ definition.title }
+        definition={ {
+          title: definition.title,
+          definition: definition.definition,
+          equations: definition.equations
+        } }
+      />
+    ) }
+  </div>
+);
 export default Glossary;
