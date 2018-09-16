5fc8b211a144d9828957c6d06a2d946f3a1d1e6c
hailey123 <hailey.musselman@gmail.com>
2018-09-16 11:55:04 -0700

    tested default definitions

--- src/client/components/glossary/Glossary.test.jsx
+++ src/client/components/glossary/Glossary.test.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { expect } from 'chai';
 
 import Glossary from '.';
+import { DEFINITIONS } from '../../components/definition/definitionData';
 
 /* eslint-disable no-undefined*/
 
 describe('<Glossary />', () => {
   it('has correct classname for styling', () => {
     const wrapper = shallow(<Glossary />);
-    expect(wrapper.hasClass('glossary')).toEqual(true);
+    expect(wrapper).to.have.className('glossary');
+  });
+  it('renders the default number of Definitions when none passed in ' +
+    'through props', () => {
+    const wrapper = shallow(<Glossary />);
+
+    expect(wrapper.find('Definition')).to.have.length(DEFINITIONS.length);
   });
   it('renders no definitions if definitions array is empty', () => {
     const mockDefinitions = [];
 
     const wrapper = shallow(<Glossary definitions={ mockDefinitions } />);
 
-    expect(wrapper.find('Definition')).toHaveLength(0);
+    expect(wrapper.find('Definition')).to.have.length(0);
   });
   it('renders 2 Definitions if definitions array has length 2', () => {
     const mockDefinitions = [{
@@ -29,7 +37,7 @@ describe('<Glossary />', () => {
 
     const wrapper = shallow(<Glossary definitions={ mockDefinitions } />);
 
-    expect(wrapper.find('Definition')).toHaveLength(2);
+    expect(wrapper.find('Definition')).to.have.length(2);
   });
   it('passes props properly to Definition it renders', () => {
     const mockDefinitions = [{
@@ -41,8 +49,8 @@ describe('<Glossary />', () => {
     const wrapper = shallow(<Glossary definitions={ mockDefinitions } />);
 
     expect(wrapper.find('Definition').first().key())
-      .toEqual('Low');
+      .to.deep.equal('Low');
     expect(wrapper.find('Definition').first().props().definition)
-      .toEqual(mockDefinitions[0]);
+      .to.deep.equal(mockDefinitions[0]);
   });
 });
