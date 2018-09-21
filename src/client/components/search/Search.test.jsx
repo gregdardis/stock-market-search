407caff0d922fcc641402364b7a2364ed3c49303
Greg Dardis <dardis.greg@gmail.com>
2018-09-21 13:19:53 -0700

    tested handleSearch in Search.jsx

--- src/client/components/search/Search.test.jsx
+++ src/client/components/search/Search.test.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import {
+  mount,
+  shallow
+} from 'enzyme';
 
 import Search from './Search';
 
 const mockFunction = () => {};
 
-const mockState = {
+const baseProps = {
   clearSearchError: mockFunction,
   clearSearchText: mockFunction,
   fetchStock: mockFunction,
@@ -14,7 +17,8 @@ const mockState = {
   stocks: {
     MSFT: {}
   },
-  text: 'A',
+  /* eslint-disable-next-line no-undefined */
+  text: undefined,
   updateSearchText: mockFunction
 };
 
@@ -25,26 +29,82 @@ describe('<Search />', () => {
 });
 
 describe('handleSearch', () => {
-  // it('should call none of it\'s functions if text is undefined', () => {
-  //   const setStockFromMemCache = jest.fn();
-  //   const fetchStock = jest.fn();
-  //   const clearSearchError = jest.fn();
+  it('should call none of it\'s functions if text is undefined', () => {
+    const setStockFromMemCache = jest.fn();
+    const fetchStock = jest.fn();
+    const clearSearchError = jest.fn();
 
-  //   const mockStateHandleSearchFunctions = {
-  //     ...mockState,
-  //     clearSearchError,
-  //     fetchStock,
-  //     setStockFromMemCache
-  //   };
+    const propsHandleSearchFunction = {
+      ...baseProps,
+      clearSearchError,
+      fetchStock,
+      setStockFromMemCache
+    };
 
-  //   const wrapper = shallow(
-  //     <Search { ...mockStateHandleSearchFunctions }/>
-  //   );
+    const wrapper = shallow(
+      <Search { ...propsHandleSearchFunction }/>
+    );
 
-  //   wrapper.instance().handleSearch();
+    wrapper.instance().handleSearch();
 
-  //   expect(setStockFromMemCache).toHaveBeenCalledTimes(0);
-  //   expect(fetchStock).toHaveBeenCalledTimes(0);
-  //   expect(clearSearchError).toHaveBeenCalledTimes(0);
-  // });
+    expect(propsHandleSearchFunction.clearSearchError)
+      .toHaveBeenCalledTimes(0);
+    expect(propsHandleSearchFunction.fetchStock)
+      .toHaveBeenCalledTimes(0);
+    expect(propsHandleSearchFunction.setStockFromMemCache)
+      .toHaveBeenCalledTimes(0);
+  });
+  it('should call correct functions if text is in stocks in state', () => {
+    const setStockFromMemCache = jest.fn();
+    const fetchStock = jest.fn();
+    const clearSearchError = jest.fn();
+
+    const propsHandleSearchFunction = {
+      ...baseProps,
+      clearSearchError,
+      fetchStock,
+      setStockFromMemCache,
+      text: 'MSFT'
+    };
+
+    const wrapper = shallow(
+      <Search { ...propsHandleSearchFunction }/>
+    );
+
+    wrapper.instance().handleSearch();
+
+    expect(propsHandleSearchFunction.clearSearchError)
+      .toHaveBeenCalledTimes(1);
+    expect(propsHandleSearchFunction.fetchStock)
+      .toHaveBeenCalledTimes(1);
+    expect(propsHandleSearchFunction.setStockFromMemCache)
+      .toHaveBeenCalledTimes(1);
+  });
+  it('should call correct functions if text is not undefined, ' +
+     'but is not a stock in the state', () => {
+    const setStockFromMemCache = jest.fn();
+    const fetchStock = jest.fn();
+    const clearSearchError = jest.fn();
+
+    const propsHandleSearchFunction = {
+      ...baseProps,
+      clearSearchError,
+      fetchStock,
+      setStockFromMemCache,
+      text: 'ABCD'
+    };
+
+    const wrapper = shallow(
+      <Search { ...propsHandleSearchFunction }/>
+    );
+
+    wrapper.instance().handleSearch();
+
+    expect(propsHandleSearchFunction.clearSearchError)
+      .toHaveBeenCalledTimes(1);
+    expect(propsHandleSearchFunction.fetchStock)
+      .toHaveBeenCalledTimes(1);
+    expect(propsHandleSearchFunction.setStockFromMemCache)
+      .toHaveBeenCalledTimes(0);
+  });
 });
