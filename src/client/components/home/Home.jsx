22e4442bc18a217358d7c66f21da36a3225c5f2a
hailey123 <hailey.musselman@gmail.com>
2018-09-23 07:49:51 -0700

    refactored search status region in Home

--- src/client/components/home/Home.jsx
+++ src/client/components/home/Home.jsx
@@ -1,35 +1,24 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 
 import Search from '../search';
 import SearchLoader from '../searchLoader';
 import StockDataRegion from '../stockDataRegion';
 import './home.css';
 
-const SEARCH_STATUS_CONTAINER_HEIGHT = 24;
-
 class Home extends Component {
-  getSearchStatusDivClassNames() {
-    const { searchError, loading } = this.props;
-    return classNames({
-      hidden: !searchError && !loading,
-      searchStatus: true
-    });
-  }
-
   render() {
     const { loading, searchError } = this.props;
     return (
       <div className='home'>
         <Search hasError={ !!searchError } />
-        <div
-          className={ this.getSearchStatusDivClassNames() }
-          style={ { height: SEARCH_STATUS_CONTAINER_HEIGHT } }>
+        <div className='searchStatus'>
           { loading
             ? <SearchLoader />
-            : <p className='error'>{ searchError }</p>
-          }
+            : null }
+          { searchError && !loading
+            ? <p className='error'>{ searchError }</p>
+            : null }
         </div>
         <StockDataRegion />
       </div>
