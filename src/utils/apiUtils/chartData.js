98d74f8db6a5dd609f9b30aa445ec855baefa409
Greg Dardis <dardis.greg@gmail.com>
2018-09-25 10:51:40 -0700

    tested getQueryForIntradayData

--- src/utils/apiUtils/chartData.js
+++ src/utils/apiUtils/chartData.js
@@ -1,4 +1,3 @@
-import flatten from 'array-flatten';
 import {
   historical,
   quote
@@ -19,107 +18,9 @@ import {
   parseDailyData
 } from '../apiUtils/responseParsing';
 import { createStock } from '../stockDataUtils/createStock';
-import {
-  formatAndAdjustDateForTimestamp,
-  getEndOfDayTimestampIndex,
-  getStartOfDayTimestampIndex,
-  getTimestampForDay
-} from './timestamps';
+import { getIntradayStockData } from './getIntradayStockData';
 
-function getDatesAndTimesForOneDay(
-  close,
-  dayIndex,
-  gmtoffset,
-  numberOfDays,
-  dateAndTimeFormat,
-  timestamp,
-  timestampIntervals
-) {
-  let datesTimesAndPrices = [];
-  const timestampsPerDay = timestamp.length / numberOfDays;
-
-  for (
-    let i = getStartOfDayTimestampIndex(dayIndex, timestampsPerDay);
-    i < getEndOfDayTimestampIndex(dayIndex, timestampsPerDay);
-    i++
-  ) {
-    if (timestamp[i] < timestampIntervals[dayIndex].start
-      || timestamp[i] > timestampIntervals[dayIndex].end) {
-      continue;
-    }
-    datesTimesAndPrices.push({
-      dateAndTime: formatAndAdjustDateForTimestamp(
-        gmtoffset, timestamp[i], dateAndTimeFormat
-      ),
-      price: close[i]
-    });
-  }
-  return datesTimesAndPrices;
-}
-
-function getTimestampIntervals(numberOfDays, meta) {
-  let timestampIntervals = [];
-  for (let dayIndex = 0; dayIndex < numberOfDays; dayIndex++) {
-    timestampIntervals.push({
-      start: getTimestampForDay(dayIndex, meta),
-      end: getTimestampForDay(dayIndex, meta, false)
-    });
-  }
-  return timestampIntervals;
-}
-
-function getDatesTimesAndPrices(
-  close,
-  gmtoffset,
-  numberOfDays,
-  dateAndTimeFormat,
-  timestamp,
-  timestampIntervals
-) {
-  let datesTimesAndPrices = [];
-  for (let dayIndex = 0; dayIndex < numberOfDays; dayIndex++) {
-    const intervalOfDatesTimesAndPrices = getDatesAndTimesForOneDay(
-      close,
-      dayIndex,
-      gmtoffset,
-      numberOfDays,
-      dateAndTimeFormat,
-      timestamp,
-      timestampIntervals
-    );
-    datesTimesAndPrices.push(intervalOfDatesTimesAndPrices);
-  }
-  return flatten(datesTimesAndPrices);
-}
-
-// numberOfDays much match the range used to obtain the intradayRes.
-function getIntradayStockData(intradayRes, numberOfDays, dateAndTimeFormat) {
-  const intradayData = JSON.parse(intradayRes);
-  const result = intradayData.chart.result[0];
-  const {
-    indicators,
-    meta,
-    timestamp
-  } = result;
-  const { gmtoffset } = meta;
-
-  // Array of objects, one for each day.
-  // Each contains a start timestamp and end timestamp for that trading day.
-  const timestampIntervals = getTimestampIntervals(numberOfDays, meta);
-
-  const { close } = indicators.quote[0];
-  const datesTimesAndPrices = getDatesTimesAndPrices(
-    close,
-    gmtoffset,
-    numberOfDays,
-    dateAndTimeFormat,
-    timestamp,
-    timestampIntervals
-  );
-  return datesTimesAndPrices;
-}
-
-function getQueryForIntradayData(symbol, range, interval) {
+export function getQueryForIntradayData(symbol, range, interval) {
   return `https://query1.finance.yahoo.com/v8/finance/chart/${symbol}` +
   `?range=${range}&includePrePost=true&interval=${interval}` +
   '&corsDomain=finance.yahoo.com&.tsrc=finance';
--- src/utils/apiUtils/chartData.test.js
+++ src/utils/apiUtils/chartData.test.js
@@ -1,9 +1,24 @@
 import { expect } from 'chai';
 
+import { getQueryForIntradayData } from './chartData';
+import { 
+  QUERY_INTERVAL_ONE_DAY,
+  QUERY_RANGE_ONE_DAY
+} from '../../constants/numeric';
+
 /* eslint-disable no-undefined */
 
-describe('tests', () => {
-  it('true equals true', () => {
-    expect(true).to.equal(true);
+describe('getQueryForIntradayData', () => {
+  it('properly returns query for intraday data', () => {
+    const mockSymbol = 'MSFT';
+    const mockRange = QUERY_RANGE_ONE_DAY;
+    const mockInterval = QUERY_INTERVAL_ONE_DAY;
+
+    expect(getQueryForIntradayData(mockSymbol, mockRange, mockInterval))
+      .to.equal(
+        `https://query1.finance.yahoo.com/v8/finance/chart/${mockSymbol}` +
+        `?range=${mockRange}&includePrePost=true&interval=${mockInterval}` +
+        '&corsDomain=finance.yahoo.com&.tsrc=finance'
+      );
   });
 });
