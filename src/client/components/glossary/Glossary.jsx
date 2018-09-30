51c6dd44e373d41ef41276864d536c354650825f
Greg Dardis <dardis.greg@gmail.com>
2018-09-30 13:42:07 -0700

    reverted Glossary to a stateless functional component now that React lifecycle methods are no longer required

--- src/client/components/glossary/Glossary.jsx
+++ src/client/components/glossary/Glossary.jsx
@@ -1,32 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import Definition from '../definition';
 import { DEFINITIONS } from '../definition/definitionData';
 import './glossary.css';
 
-class Glossary extends Component {
-  render() {
-    return (
-      <div className='glossary'>
-        { this.props.definitions.map(definition =>
-          <Definition
-            key={ definition.title }
-            definition={ {
-              title: definition.title,
-              definition: definition.definition,
-              equations: definition.equations
-            } }
-          />
-        ) }
-      </div>
-    );
-  }
-}
+const Glossary = ({ definitions = DEFINITIONS }) => (
+  <div className='glossary'>
+    { definitions.map(definition =>
+      <Definition
+        key={ definition.title }
+        definition={ {
+          title: definition.title,
+          definition: definition.definition,
+          equations: definition.equations
+        } }
+      />
+    ) }
+  </div>
+);
 Glossary.propTypes = {
   definitions: PropTypes.array
-};
-Glossary.defaultProps = {
-  definitions: DEFINITIONS
 };
 export default Glossary;
