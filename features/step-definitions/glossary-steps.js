ad2f29a69176ff03d651cb6c45beb881a7dd5ab3
hailey123 <hailey.musselman@gmail.com>
2018-10-07 13:01:26 -0600

    wrote code to test glossary is present

--- features/step-definitions/glossary-steps.js
+++ features/step-definitions/glossary-steps.js
@@ -2,15 +2,14 @@
 
 module.exports = function () {
   this.Given(/^I am on the homepage$/, () => {
-    // TODO
-    return helpers.loadPage(shared.constants.url).then(() => {
-
-    });
+    return helpers.loadPage(shared.constants.url);
   });
   this.When(/^I click the glossary button in the menu$/, () => {
-    // TODO
+    return driver.findElement(by.linkText('Glossary')).then(link => {
+      return link.click();
+    });
   });
-  this.Then(/^I should see the glossary$/, function () {
-    // TODO
+  this.Then(/^I should see the glossary$/, () => {
+    return driver.findElement(by.css('.glossary'));
   });
 };
