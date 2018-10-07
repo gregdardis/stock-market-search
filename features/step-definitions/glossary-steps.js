69336c2e38f08b45b922fe5c477fef843e58874a
hailey123 <hailey.musselman@gmail.com>
2018-10-07 12:19:09 -0600

    opening correct url in Given, ignored files

--- features/step-definitions/glossary-steps.js
+++ features/step-definitions/glossary-steps.js
@@ -3,6 +3,9 @@
 module.exports = function () {
   this.Given(/^I am on the homepage$/, () => {
     // TODO
+    return helpers.loadPage(shared.constants.url).then(() => {
+
+    });
   });
   this.When(/^I click the glossary button in the menu$/, () => {
     // TODO
